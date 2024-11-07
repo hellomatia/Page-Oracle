ade892bfa994e385a7aeca9c1404344627e3c556
hellomatia <shuaigejp80@gmail.com>
2024-11-08 00:46:57 +0900

    feat: 데이터들은 json파일로 분리하여, 동적으로 화면을 만들 수 있는 기능 추가

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -1,26 +1,47 @@
 import React, { useRef } from 'react';
 import HTMLFlipBook from 'react-pageflip';
 import RandomPageButton from './RandomPageButton'; // 랜덤 페이지 버튼 임포트
-import { EmptyPage, ContentPage, CoverPage } from './PageComponents';
+import { EmptyPage, ContentPage, CoverPage } from './PageComponents'; // 컴포넌트 임포트
 import './Book.css';
+import bookData from '../data/BookData.json'; // JSON 데이터 가져오기
 
 function Book() {
     const flipBookRef = useRef(null); // FlipBook 참조 생성
-    const totalPages = 8; // 총 페이지 수
+    const totalPages = bookData.length * 2; // 총 페이지 수
 
     const goToRandomPage = () => {
-        const randomPage = Math.floor(Math.random() * totalPages * 2) / 2; // 0에서 totalPages까지 랜덤 페이지 생성
+        const randomPage = Math.floor(Math.random() * totalPages); // 랜덤 페이지 계산
         if (flipBookRef.current) {
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-expect-error
             flipBookRef.current.pageFlip().flip(randomPage); // 랜덤 페이지로 이동
         }
     };
 
-    const pageRefs = {
-        emptyPage: useRef<HTMLDivElement>(null),
-        contentPage: useRef<HTMLDivElement>(null),
-        coverPage: useRef<HTMLDivElement>(null),
+    // const pageRefs = {
+    //     emptyPages: useRef([]),  // EmptyPage들을 위한 배열 ref
+    //     contentPages: useRef([]), // ContentPage들을 위한 배열 ref
+    //     coverPages: useRef([]),   // CoverPage들을 위한 배열 ref
+    // };
+
+    // JSON 데이터 기반으로 페이지 생성 함수
+    const createBookPages = () => {
+        const pages = [];
+        console.log("Creating pages...");
+
+        // CoverPage 추가
+        pages.push(<CoverPage key="cover-top" title="책님..." isCoverTop />);
+
+        // bookData로 페이지 추가
+        bookData.forEach((page, index) => {
+            pages.push(<EmptyPage key={`empty-${index}`} />);
+            pages.push(<ContentPage key={`content-${index}`} text={page.text} />);
+        });
+
+        // 마지막 CoverPage 추가
+        pages.push(<CoverPage key="cover-bottom" title="끝..." isCoverBottom />);
+        console.log(pages);
+
+        // Fragment로 감싸서 반환
+        return <>{pages}</>;
     };
 
     return (
@@ -29,44 +50,21 @@ function Book() {
                           autoSize={true}
                           width={550}
                           height={733}
-
                           size={"stretch"}
                           minWidth={315}
                           maxWidth={1000}
                           minHeight={420}
                           maxHeight={1350}
-
                           maxShadowOpacity={0.5}
                           showCover={true}
                           mobileScrollSupport={false}
                           ref={flipBookRef}>
-                <CoverPage ref={pageRefs.coverPage} title="책님..." isCoverTop />
-                <EmptyPage ref={pageRefs.emptyPage} />
-                <ContentPage
-                    ref={pageRefs.contentPage}
-                    text="기다리면 된다."
-                />
-                <EmptyPage ref={pageRefs.emptyPage} />
-                <ContentPage
-                    ref={pageRefs.contentPage}
-                    text="포기해라."
-                />
-                <EmptyPage ref={pageRefs.emptyPage} />
-                <ContentPage
-                    ref={pageRefs.contentPage}
-                    text="무조건 믿어라"
-                />
-                <EmptyPage ref={pageRefs.emptyPage} />
-                <ContentPage
-                    ref={pageRefs.contentPage}
-                    text="반대로 생각해라"
-                />
-                <CoverPage ref={pageRefs.coverPage} title="끝..." isCoverBottom />
+                {/*{createBookPages()} /!* 생성한 페이지들 렌더링 *!/*/}
             </HTMLFlipBook>
 
-            <br></br>
-            <br></br>
-            <br></br>
+            <br />
+            <br />
+            <br />
 
             <div className="buttonContainer">
                 <RandomPageButton onClick={goToRandomPage}/> {/* 랜덤 페이지 버튼 추가 */}
